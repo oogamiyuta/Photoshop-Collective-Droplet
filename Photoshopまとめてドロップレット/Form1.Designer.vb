9a06e945a9dde2f4d4b2cda4fded6e43987082fa
大狼ユタ <yuta.vtuber@gmail.com>
2025-02-19 05:09:28 +0900

    プロジェクト ファイルを追加します。

---
+++ Photoshopまとめてドロップレット/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ComboBox1 = New ComboBox()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        ListBox2 = New ListBox()
+        BtnAddPath = New Button()
+        BtnRemovePath = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(2, 1)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(409, 23)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.AllowDrop = True
+        ListBox1.Dock = DockStyle.Bottom
+        ListBox1.FormattingEnabled = True
+        ListBox1.HorizontalScrollbar = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(0, 97)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(800, 379)
+        ListBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(632, 0)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "スタート"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(713, 1)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "クリア"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox2
+        ' 
+        ListBox2.FormattingEnabled = True
+        ListBox2.ItemHeight = 15
+        ListBox2.Location = New Point(0, 45)
+        ListBox2.Name = "ListBox2"
+        ListBox2.Size = New Size(800, 34)
+        ListBox2.TabIndex = 4
+        ' 
+        ' BtnAddPath
+        ' 
+        BtnAddPath.Location = New Point(417, 1)
+        BtnAddPath.Name = "BtnAddPath"
+        BtnAddPath.Size = New Size(99, 23)
+        BtnAddPath.TabIndex = 5
+        BtnAddPath.Text = "ドロップレット登録"
+        BtnAddPath.UseVisualStyleBackColor = True
+        ' 
+        ' BtnRemovePath
+        ' 
+        BtnRemovePath.Location = New Point(522, 0)
+        BtnRemovePath.Name = "BtnRemovePath"
+        BtnRemovePath.Size = New Size(104, 23)
+        BtnRemovePath.TabIndex = 6
+        BtnRemovePath.Text = "ドロップレット削除"
+        BtnRemovePath.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(2, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(25, 15)
+        Label1.TabIndex = 7
+        Label1.Text = "ログ"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(2, 82)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(204, 15)
+        Label2.TabIndex = 8
+        Label2.Text = "↓ドラッグアンドドロップで画像ファイル追加"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 476)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(BtnRemovePath)
+        Controls.Add(BtnAddPath)
+        Controls.Add(ListBox2)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Controls.Add(ComboBox1)
+        Name = "Form1"
+        Text = "Photoshopまとめてドロップレット"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents BtnAddPath As Button
+    Friend WithEvents BtnRemovePath As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Photoshopまとめてドロップレット/Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports System.IO
+Imports System.Diagnostics
+Imports System.Speech.Synthesis
+Public Class Form1
+    Private synthesizer As New SpeechSynthesizer()
+    Private settingsFile As String = Path.Combine(Application.UserAppDataPath, "settings.csv")
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadPaths()
+    End Sub
+
+    Private Sub LoadPaths()
+        ComboBox1.Items.Clear()
+        If File.Exists(settingsFile) Then
+            Dim lines = File.ReadAllLines(settingsFile)
+            For Each line In lines
+                ComboBox1.Items.Add(line)
+            Next
+        End If
+        If ComboBox1.Items.Count > 0 Then ComboBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub SavePaths()
+        File.WriteAllLines(settingsFile, ComboBox1.Items.Cast(Of String)())
+    End Sub
+
+    Private Sub BtnAddPath_Click(sender As Object, e As EventArgs) Handles BtnAddPath.Click
+        Using ofd As New OpenFileDialog()
+            ofd.Filter = "ドロップレット (*.exe)|*.exe"
+            ofd.Title = "追加するドロップレットファイルを選択"
+            If ofd.ShowDialog() = DialogResult.OK Then
+                If Not ComboBox1.Items.Contains(ofd.FileName) Then
+                    ComboBox1.Items.Add(ofd.FileName)
+                    SavePaths()
+                End If
+            End If
+        End Using
+    End Sub
+
+    Private Sub BtnRemovePath_Click(sender As Object, e As EventArgs) Handles BtnRemovePath.Click
+        If ComboBox1.SelectedIndex >= 0 Then
+            ComboBox1.Items.RemoveAt(ComboBox1.SelectedIndex)
+            SavePaths()
+        End If
+    End Sub
+
+    Private Sub ListBox1_DragDrop(sender As Object, e As DragEventArgs) Handles ListBox1.DragDrop
+        Dim files As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
+        For Each file In files
+            If Directory.Exists(file) Then
+                ListBox1.Items.Add(file) ' フォルダーもリストに追加
+            ElseIf file.ToLower().EndsWith(".jpg") OrElse file.ToLower().EndsWith(".png") OrElse file.ToLower().EndsWith(".bmp") OrElse file.ToLower().EndsWith(".tiff") Then
+                ListBox1.Items.Add(file)
+            End If
+        Next
+    End Sub
+
+    Private Sub ListBox1_DragEnter(sender As Object, e As DragEventArgs) Handles ListBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dropletPath As String = ComboBox1.SelectedItem.ToString()
+        Dim folderGroups As New Dictionary(Of String, List(Of String))()
+        ListBox2.Items.Clear()
+        For Each item As String In ListBox1.Items
+            If File.Exists(item) Then
+                Dim parentFolder As String = Directory.GetParent(item).FullName
+                If Not folderGroups.ContainsKey(parentFolder) Then
+                    folderGroups(parentFolder) = New List(Of String)()
+                End If
+                folderGroups(parentFolder).Add(item)
+            End If
+        Next
+
+        For Each folder In folderGroups.Keys
+            ExecuteCommand(dropletPath, folderGroups(folder))
+            ListBox2.Items.Add($"{Now().ToString} 処理が完了しました: {folder}")
+            SpeakNotification($"処理が完了しました: {folder}")
+        Next
+
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub ExecuteCommand(dropletPath As String, imageFiles As List(Of String))
+        Dim droplet As String = Chr(34) & dropletPath & Chr(34)
+        Dim command As String
+        For Each file In imageFiles
+            command &= " " & Chr(34) & file & Chr(34)
+        Next
+
+        command = command.TrimStart()
+
+        Dim process As New Process()
+        process.StartInfo.FileName = droplet
+        process.StartInfo.Arguments = command
+        process.StartInfo.UseShellExecute = False
+        process.StartInfo.CreateNoWindow = True
+        process.Start()
+        process.WaitForExit()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+    End Sub
+    ' 音声通知を生成して再生
+    Private Sub SpeakNotification(message As String)
+        Try
+            synthesizer.SpeakAsync(message)
+        Catch ex As Exception
+            MessageBox.Show("音声通知の生成中にエラーが発生しました。", "エラー")
+        End Try
+    End Sub
+
+End Class
